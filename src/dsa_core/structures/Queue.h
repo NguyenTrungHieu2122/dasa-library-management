0f54ed8bb4deb5451d5efaa5bcab848d267ad6d4
Trương Hoàng Phúc <25110306@student.hcmute.edu.vn>
2026-09-20 18:40:35 +0700

    Update Queue.h

--- src/dsa_core/structures/Queue.h
+++ src/dsa_core/structures/Queue.h
@@ -1,15 +1,41 @@
+
 #pragma once
 #include<queue>
+#include<stdexcept>
 template <typename T>
 class Queue {
 private:
-	std::queue<T> container;
+	std::queue<T> q;
 public:
 	Queue() = default;
-	void enqueue(const T& item);//thêm phần tử vào cuối hàng đợi
-	void dequeue();// lấy và xóa phần tử đầu của hàng đợi
-	T& front();//lấy phần tử đầu
-	T& back();//lấy phần tử cuối
-	bool isEmpty() const;//kiểm tra queue có trống không
-	int size() const;//lấy số phần tử trong queue
+	void enqueue(const T& item)//thêm phần tử vào cuối hàng đợi
+	{
+		q.push(item);
+	}
+	T dequeue()// lấy và xóa phần tử đầu của hàng đợi
+	{
+		if (q.empty()) {
+			throw std::runtime_error("Queue is empty");
+		}
+		T mem = q.front();
+		q.pop();
+		return mem;
+	}
+	T& front()//lấy phần tử đầu
+	{
+		return q.front();
+	}
+	T& back()//lấy phần tử cuối
+	{
+		return q.back();
+	}
+	bool isEmpty() const//kiểm tra queue có trống không
+	{
+		return q.empty();
+	}
+	int size() const//lấy số phần tử trong queue
+	{
+		return q.size();
+	}
 };
+
