6444903e02163247aea33e7efe9def86aaf73372
25110306-bip <25110306@student.hcmute.edu.vn>
2026-09-22 17:19:54 +0700

    update

--- src/dsa_core/services/ReservationService.h
+++ src/dsa_core/services/ReservationService.h
@@ -1,11 +1,10 @@
 #pragma once
 #include <string>
 #include "../models/Reservation.h"
-class ReservationService {
-    public:
-    void addmember(reservation& res, const std::string memberId, const std::string& reservedAt);
-    reservationEntry removeMember(reservation& res);
-    reservationEntry getFirstMember(reservation& res);
-    bool isempty(reservation& res);
-    int size(reservation& res);
-};
+
+void addmember(reservation& res, const std::string memberId, const std::string& reservedAt);
+reservationEntry removeMember(reservation& res);
+reservationEntry getFirstMember(reservation& res);
+bool isempty(reservation& res);
+int size(reservation& res);
+
