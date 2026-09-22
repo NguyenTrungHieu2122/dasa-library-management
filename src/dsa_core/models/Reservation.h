948fd0bfded0a86e1d4527df1a58d67de0efa552
Trương Hoàng Phúc <25110306@student.hcmute.edu.vn>
2026-09-22 14:47:28 +0700

    Update Reservation.h

--- src/dsa_core/models/Reservation.h
+++ src/dsa_core/models/Reservation.h
@@ -2,19 +2,14 @@
 #include <string>
 #include "../structures/Queue.h"
 
-struct reservationEntry {
+struct reservationEntry
+{
     std::string memberId;
     std::string reservedAt;
 };
-class reservation {
-private:
+
+struct reservation
+{
     std::string bookId;
     Queue<reservationEntry> q;
-public:
-    reservation(std::string id);
-    std::string getbook() const;
-    void addmember(std::string memberId,std string reservedAt);
-    reservationentry removemember();
-    bool isempty() const;
-    int size() const;
 };
